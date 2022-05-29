b4200bd0a0997b86e9c53b74c1f537d489d462be
Dauda Adbulganiyu <daudakale@gmail.com>
2022-05-29 21:48:38 +0100

    front end started

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,3 +2,5 @@ export const DB_CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING ||
   "mongodb+srv://agastech:agastech123@cluster0.rnejn3f.mongodb.net/?retryWrites=true&w=majority";
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+
+export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || "localhost";
